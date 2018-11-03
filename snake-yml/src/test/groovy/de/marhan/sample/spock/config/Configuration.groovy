edefdc447b666b41dd4e7b9c203c0e8042184af4
Markus Hanses <markus.hanses@immonet.de>
2018-11-03 23:15:59 +0100

    snakeyaml configuration incompatibility with Java 10 solved.

--- snake-yml/src/test/groovy/de/marhan/sample/spock/config/Configuration.groovy
+++ snake-yml/src/test/groovy/de/marhan/sample/spock/config/Configuration.groovy
@@ -7,9 +7,8 @@ class Configuration {
     Map<String, ServiceConfig> services
 
     static Configuration init() {
-        def yaml = new Yaml()
-        def yamlDocument = this.getClass().getResource("/${System.getProperty("environment")}.yml").getText('UTF-8')
-        yaml.loadAs(yamlDocument, Configuration.class)
+		String configFile = new File("src/test/resources/${System.getProperty("environment")}.yml").toURI().toURL().getText("UTF-8")
+		new Yaml().loadAs(configFile, Configuration.class)
     }
 
 }
